f4b219ccd9593ca70636bdbfd06b2e032b9c7593
jkk111 <kellyj58@tcd.ie>
2018-06-13 00:37:51 +0100

    Fixed Round Tripper

--- proxy/round-tripper_other.go
+++ proxy/round-tripper_other.go
@@ -5,10 +5,9 @@ package proxy
 import (
   "net"
   "context"
-  "strings"
   "github.com/jkk111/indigo/sockets"
 )
 
 func (this * BetterRoundTripper) socketRoundTrip(ctx context.Context, network string, addr string) (c net.Conn, err error) {
-  sockets.Dial(addr)
+  return sockets.Dial(addr)
 }

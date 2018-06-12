e54036eee999aee5e2f06e215e4a360d28d62bcb
jkk111 <kellyj58@tcd.ie>
2018-06-13 00:35:58 +0100

    removed unused vars

--- sockets/sockets_other.go
+++ sockets/sockets_other.go
@@ -12,7 +12,7 @@ const TIMEOUT = time.Second * 30
 
 func Dial(path string) (net.Conn, error) {
   path = strings.Replace(path, ":80", "", -1)
-  timeout := TIMEOUT
+  // timeout := TIMEOUT
   return net.Dial("unix", path) 
 }
 
